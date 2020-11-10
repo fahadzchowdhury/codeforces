c115d4ae4f9a482a026a572a4d2140bf5d8f4b59
Fahad Zaman Chowdhury <fahadzchowdhury@outlook.com>
2020-11-10 15:12:43 +0600

    Adding updated solution of: Next round

--- next_round.cpp
+++ next_round.cpp
@@ -9,13 +9,22 @@ int main(void)
     {
         cin>>array[i];
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < k; i++)
     {
-        if (array[i]!=k && array[i]>0)
+        if (array[i]>=array[k-1] && array[i]>0)
         {
             counter++;
         }
     }
+    for (int i = k; i < n; i++)
+    {
+        if (array[i]==array[k-1] && array[i]>0)
+        {
+            counter++;
+        }
+        
+    }
+    
     cout<<counter<<endl;
     
 }
